450c3b183fd033f689d1e945d7e0314819bf2ba6
clark-cui <cuirongchuan@163.com>
2022-05-04 18:26:51 +0800

    fix: actionjs local

--- scripts/action.js
+++ scripts/action.js
@@ -4,18 +4,21 @@ const {
 const CounterArtifact = require("../data/Counter.json")
 const contractAddress = require("../data/contract-address.json");
 
-// init
-const provider = new ethers.providers.Web3Provider(window.ethereum);
+(async () => {
+  // init
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/');
 
-// Then, we initialize the contract using that provider and the token's
-// artifact. You can do this same thing with your contracts.
-const counter = new ethers.Contract(
-  contractAddress.Counter,
-  CounterArtifact.abi,
-  provider.getSigner(0)
-);
+  // Then, we initialize the contract using that provider and the token's
+  // artifact. You can do this same thing with your contracts.
+  const counter = new ethers.Contract(
+    contractAddress.Counter,
+    CounterArtifact.abi,
+    provider.getSigner(0)
+  );
 
-console.log("counter值是：", counter.counter())
+  console.log("counter值是：",await counter.counter())
 
-await counter.count();
-console.log("counter值是：", counter.counter())
+  await counter.count();
+  console.log("counter值是：",await counter.counter())
+
+})()
