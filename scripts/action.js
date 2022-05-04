089cb61a235c798a69366af50281014f31799ea9
clark-cui <cuirongchuan@163.com>
2022-05-04 14:45:39 +0800

    feature: add action

--- scripts/action.js
+++ scripts/action.js
@@ -1,11 +1,15 @@
-const {
-  ethers,
-  network
-} = require("hardhat");
-const Addr = require("../abi/Counter.json");
-async function main() {
-  const [deployer] = await ethers.getSigners();
-  let counter = await ethers.getContractAt("Counter", Addr.address, deployer);
-  let newValue = await counter.counter();
-  console.log("value is:", newValue);
-}
+const fs = require('fs');
+const abiFile = JSON.parse(fs.readFileSync('./abi/Counter.json'))
+const Web3 = require("web3");
+// 创建web3对象
+const web3 = new Web3();
+// 连接到以太坊节点
+web3.setProvider(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+
+const abi = abiFile
+// 合约地址
+const address = "0xb2cdd356e58280906ce53e1665697b50f88aac56";
+// 通过ABI和地址获取已部署的合约对象
+const counter = web3.eth.contract(abi).at(address);
+const balance = counter.getBalance;
+console.log(" balance: ", balance);
