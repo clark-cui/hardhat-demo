f15debe3457341a778babe58c15459dce19ce5af
clark-cui <cuirongchuan@163.com>
2023-02-18 22:50:09 +0800

    feat: tutorial test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## hardhat
+
+### setup
+
+https://hardhat.org/tutorial/writing-and-compiling-contracts
--- hardhat-tutorial/contracts/SimpleToken.sol
+++ hardhat-tutorial/contracts/SimpleToken.sol
@@ -4,6 +4,9 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
+// for debug 
+import "hardhat/console.sol";
+
 
 // This is the main building block for smart contracts.
 contract SimpleToken {
@@ -45,6 +48,14 @@ contract SimpleToken {
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        // should import "hardhat/console.sol" before console
+        console.log(
+        "Transferring from %s to %s %s tokens",
+        msg.sender,
+        to,
+        amount
+        );
 
         // Transfer the amount.
         balances[msg.sender] -= amount;
